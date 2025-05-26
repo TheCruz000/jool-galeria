3549ac30f8ad38ef950715b1e3f948629255267f
Alejandro Cruz Cerna Araiza <nohizelatarea@gmail.com>
2025-05-26 05:54:00 -0600

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; 
 
 export default defineConfig({
   plugins: [react()],
   base: '/jool-galeria/',
   server: {
-    host: '0.0.0.0' // Para acceder desde la red local
+    host: '0.0.0.0' // Mantén esto para desarrollo
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
+    assetsDir: 'assets', // Opcional (se sobreescribe por rollupOptions)
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      },
+      output: {
+        entryFileNames: 'assets/main-[hash].js', // Nombre controlado
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
-});
+})
